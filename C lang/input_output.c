6c893d244274c27eb33f0eb6d20b0a293f8142e7
sankalp kumar <sankalp05oct@gmail.com>
2024-03-31 22:44:05 +0530

    gitignore

--- C lang/input_output.c
+++ C lang/input_output.c
@@ -9,7 +9,7 @@ int main() {
 	// printf("\\r");
 	// printf("\nTeachers \nDay");
 
-	int num1, num2;
+	// int num1, num2;
 	// printf("enter two integers to get it's sum: ");
 	// scanf("%d%d", &num1, &num2);
 	// printf("\n%d+%d=%d", num1, num2, num1 + num2);
@@ -23,11 +23,44 @@ int main() {
 	// scanf("%d %d", &l, &w);
 	// printf(" the area of rectange is %d \n", l * w);
 
-	int r;
-	printf("enter the radus of circle: ");
-	scanf("%d",&r);
-	float radius = 3.1415 * (r * r);
-	printf("Area of circle is %d having the radius %f", r, radius);
-	printf("\n");
+	// int r;
+	// printf("enter the radus of circle: ");
+	// scanf("%d",&r);
+	// float radius = 3.1415 * (r * r);
+	// printf("Area of circle is %d having the radius %f", r, radius);
+
+	// int a, b, c;
+	// float avg;
+	// printf("enter three numbers: ");
+	// scanf("%d%d%d", &a, &b, &c);
+	// avg = (a + b + c) / 3;
+	// printf("the average is: %f ", avg);
+
+	// int r;
+	// float PI = 3.1415, CF;
+	// printf("enter the radius of circle: ");
+	// scanf("%d", &r);
+	// CF = 2 * (PI * r);
+	// printf(" the circumference of circle is: %f", CF);
+
+	// int p, r, t;
+	// float SI;
+	// printf("enter principal, rate of intrest and time period: ");
+	// scanf("%d%d%d", &p, &r, &t);
+	// SI = (p * r * t) / 100;
+	// printf("the simple intrest is: %f", SI);
+
+	// int l, w, h;
+	// printf("enter length, width and height");
+	// scanf("%d%d%d", &l, &w, &h);
+	// printf(" the volume of cuboid is: %d", l * w * h);
+	// printf("\n");
+
+	// float cp, sp, profit;
+	// printf("enter the cost price and selling price of bnana per deson: ");
+	// scanf("%f%f", &cp, &sp);
+	// profit = (sp - cp) * 25 / 12;
+	// printf(" proft earnd upto selling 25 banana is %.3f\n", profit);
+
 	return 0;
 }
