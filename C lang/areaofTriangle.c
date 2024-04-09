57e4a00c58477c680142c6f0789fca17149efb5a
sankalp kumar <sankalp05oct@gmail.com>
2024-04-09 17:20:14 +0530

    modify operaters.c

---
+++ C lang/areaofTriangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int  main(void) {
+
+	float a, b, c, sp, area;
+	printf("enrter the three sides of triange: ");
+	scanf("%f%f%f", &a, &b, &c);
+
+	sp = (a + b + c) / 2;
+	area = sqrt(sp * (sp - a) * (sp - b) * (sp - c));
+
+	printf(" the area of triangle is : %f",area);
+
+
+	printf("\n");
+	return 0;
+}
---
+++ C lang/operaters.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int  main(void) {
+
+	int x = 5, y;
+	// printf("%d", x++);
+	// printf(" %d", x);
+	y = ++x;
+	printf("%d, %d", x,y);
+	printf("\n");
+	return 0;
+}
