d63fc8922d42b49d3dd29729d5791638d13320ad
sankalp kumar <sankalp05oct@gmail.com>
2024-04-15 13:36:15 +0530

    learnd compilation stage

---
+++ cpp/how_compiler_works/Muldif-Mul.s
@@ -0,0 +1,1 @@
+	.file	"Mul.h"
---
+++ cpp/how_compiler_works/first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Mul.h"
+
+
+
+int main() {
+	// std::cout << "hello world" << std::endl;
+    int x = Multiply(23, 45);
+	std::cout << x << std::endl;
+	// std::cin.get();
+
+}
