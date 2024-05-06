cde38b5a8abe9fb135ccf42d7943617f33a2b635
sankalp kumar <sankalp05oct@gmail.com>
2024-05-06 21:22:05 +0530

    added some c files

--- C lang/07 Arrays in c/assignment18.c
+++ C lang/07 Arrays in c/assignment18.c
@@ -2,21 +2,30 @@
 
 
 
+
 void scanArray(int arr[], int size) {
-	for(int i=1; i<=size; i++) {
+	for(int i=0; i<=size-1; i++) {
 		printf("~ scan for array index %d: ",i);
 		scanf("%d", &arr[i]);
 	}
 }
 
 
+// arr[1,4,5,0,45,6]
 int getLargets(int arr[], int size) {
-	for(int i=1; i<=size; i++) {
-		
+	int k=0, max=arr[0];
+	for(int i=0; i<=size-1; i++) {
+        for(int j=k+1; j<= size-1; j++) {
+			// printf("i: %d, j: %d, k: %d, max: %d, arr[j]: %d\n",i,j,k,max,arr[j]);
+			if(max<arr[j]) {
+				max = arr[j];
+				k=j;
+				i=k;
+			}
+		}
 	}
+    return max;
 }
-
-
 
 int  main(void) {
 
